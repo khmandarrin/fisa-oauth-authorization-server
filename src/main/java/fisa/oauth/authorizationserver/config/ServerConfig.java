87bb39ffbe7fb146907525f32adf6d0073c8e7a1
khmandarrin <jeong-ga-eun@jeong-ga-eun-ui-MacBookAir.local>
2026-03-12 16:58:22 +0900

    first commit

---
+++ src/main/java/fisa/oauth/authorizationserver/config/ServerConfig.java
@@ -0,0 +1,16 @@
+package fisa.oauth.authorizationserver.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+public class ServerConfig {
+
+    // 인가 서버 보안 설정
+
+    // issuer 설정
+
+    // 클라이언트 등록 설정
+
+    // JWKSource 설정
+
+}
